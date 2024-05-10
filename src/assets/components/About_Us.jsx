c1acf64f6523f22aaf97ed2dfdd77186068b2877
Elmira Loghmani <elmiiiraloq@gmail.com>
2024-05-10 13:43:44 +0330

    Update About_Us.jsx

--- src/assets/components/About_Us.jsx
+++ src/assets/components/About_Us.jsx
@@ -14,13 +14,9 @@ const AboutUs = () => {
                     <div className="w-[100%] bg-white h-[1.5px]"></div>
 
                 </h1>
-                <p className="text-white text-[15px] text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing
-                    elit. Debitis, dignissimos fugit laborum magnam nisi omnis sequi ut velit. Pariatur porro
-                    praesentium rerum saepe sint tempore ullam velit. A amet, mollitia.Lorem ipsum dolor sit amet,
-                    consectetur adipisicing elit. Debitis, dignissimos fugit laborum magnam nisi omnis sequi ut velit.
-                    Pariatur porro praesentium rerum saepe sint tempore ullam velit. A amet, mollitia.Lorem ipsum dolor
-                    sit amet, consectetur adipisicing elit. Debitis, dignissimos fugit laborum magnam nisi omnis sequi
-                    ut velit.
+                <p className="text-white text-[15px] text-justify">
+                    We are the Jollypop team and we are engaged in the production and distribution of popcorn. We started our activity in 2019 and until now, we have gone through many things and faced many difficulties. We always said that we are the best in this profession, do you need proof? So order our products right now to find out for yourself!
+                    You can find any kind of popcorn with any flavor you want here, new things that you have never seen anywhere, so don't wait! Register on our site right now and place your order, so you can see the product in front of you and taste it. Welcome to Jollypop popcorn paradise!
                 </p>
             </div>
         </div>
