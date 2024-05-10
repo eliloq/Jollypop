7b62025ef875416624dabc1df92ae0ca7f9daff9
Elmira Loghmani <elmiiiraloq@gmail.com>
2024-05-10 13:44:14 +0330

    Update Home.jsx

--- src/assets/components/landingPage/Home.jsx
+++ src/assets/components/landingPage/Home.jsx
@@ -4,7 +4,7 @@ import Slider from "./Slider";
 
 const Home = () => {
     return (
-        <div className="w-full xl:h-[800px] lg:h-[750px] md:h-[650px] sm:h-[1100px] h-[850px] bg-[url('./assets/images/Vector.png')] bg-no-repeat bg-center bg-auto flex md:flex-row flex-col items-center md:justify-evenly md:gap-0 gap-6">
+        <div className="w-full xl:h-[800px] lg:h-[750px] md:h-[650px] sm:h-[1200px] bg-[url('./assets/images/vector.png')] bg-no-repeat md:bg-center bg-top flex md:flex-row flex-col items-center md:justify-evenly md:gap-0 gap-6">
             <Content/>
             <Slider/>
         </div>
